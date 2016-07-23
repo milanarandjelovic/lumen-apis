0c00d1deaee7edc0bb8ab12a46bdcb203648af44
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2016-07-23 16:11:00 +0200

    Added test for Book model

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -3,14 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use Illuminate\Http\Request;
 
 class BooksController extends Controller
 {
 
+    /**
+     * Return all books.
+     * @return json
+     */
     public function index()
     {
         $books = Book::all(); // all books form database
         return response()->json($books);
+    }
+
+    /**
+     * Return book by $id
+     * @param  int $id
+     * @return json
+     */
+    public function show($id)
+    {
+        try {
+            return Book::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => [
+                    'message' => 'Book not found'
+                ]
+            ], 404);
+        }
+    }
+
+    /**
+     * Save book
+     * @param  Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function store(Request $request)
+    {
+        $book = Book::create($request->all());
+        return response()->json(['created' => true], 201, [
+            'Location' => route('books.show', ['id' => $book->id])
+        ]);
     }
 
 }
